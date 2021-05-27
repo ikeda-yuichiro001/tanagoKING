459f28783a9c7f6670bd980b4ff6c48c94291704
DESKTOP-B2QSEQC\nanas <e214122sk@kurume-it.ac.jp>
2021-05-27 16:03:53 +0900

    猿渡 タイトルとゲームセレクト

---
+++ tanagoking/Assets/selectsystem.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class selectsystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //わからんからとりあえず変数宣言
+        int point1;
+
+        point1 = 0;
+
+
+        //方向キー下
+        if (point1 == 0 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            point1 = 1;
+
+        }
+        if (point1 == 1 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            point1 = 2;
+
+        }
+        if (point1 == 2 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            point1 = 3;
+
+        }
+        if (point1 == 3 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            point1 = 0;
+
+        }
+
+        //方向キー上
+        if (point1 == 0 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            point1 = 3;
+
+        }
+        if (point1 == 1 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            point1 = 0;
+
+        }
+        if (point1 == 2 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            point1 = 1;
+
+        }
+        if (point1 == 3 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            point1 = 2;
+
+        }
+
+        //方向キー左右
+        if (point1 <= 2  && Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            point1 = 3;
+
+        }
+        if (point1 == 3 && Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            point1 = 0;
+
+        }//シーン移動(一時的に３と０の結果を入れ替える)
+        if (point1 == 3 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("nurturing");
+        }
+        if (point1 == 1 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("off-line");
+        }
+        if (point1 == 2 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("on-line");
+        }
+        if (point1 == 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("title");
+        }
+
+        //if (point1 == 0 && )
+        {
+
+        }
+        //if (point1 == 1 && )
+        {
+
+        }
+        //if (point1 == 2 && )
+        {
+
+        }
+        //if (point1 == 3 && )
+        {
+
+        }
+
+
+    }
+}
---
+++ tanagoking/Assets/titlesystem.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titlesystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        
+    }
+
+
+
+// Update is called once per frame
+void Update()
+    {
+        //わからんからとりあえず変数宣言
+        int point;
+
+        point = 0;
+
+
+        //方向キー下
+        if (point == 0 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            point = 1;
+
+        }
+        if (point == 1 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            point = 2;
+
+        }
+        if (point == 2 && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            point = 0;
+
+        }
+
+        //方向キー上
+        if (point == 0 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            point = 2;
+
+        }
+        if (point == 1 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            point = 0;
+
+        }
+        if (point == 2 && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            point = 1;
+
+        }
+
+        //シーン移動
+        if (point == 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("select");
+        }
+        if (point == 1 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("option");
+        }
+        if (point == 2 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("gallery");
+        }
+        // SceneManager.LoadScene("行きたいシーンの名前");
+
+    }
+}
