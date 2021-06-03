0d9a2c083644205be889cda944cff2820b209e15
DESKTOP-B2QSEQC\nanas <e214122sk@kurume-it.ac.jp>
2021-06-03 11:58:55 +0900

    ボタンの複数管理の糸口

--- Assets/clickscript.cs
+++ Assets/clickscript.cs
@@ -22,9 +22,24 @@ public class clickscript : MonoBehaviour
     }
     
         // ボタンが押された場合、今回呼び出される関数
-        public void OnClick()
+        public void OnClick(int nunber)
         {
           SceneManager.LoadScene("select");
+        switch (nunber)
+        {
+            case 0:
+                SceneManager.LoadScene("select");
+                break;
+            case 1:
+                SceneManager.LoadScene("option");
+                break;
+            case 2:
+                SceneManager.LoadScene("gallery");
+                break;
+            default:
+                break;
         }
+    }
+
     
 }
