4d8570b04c8c99cb4cc54d49c1485ce375a54f58
DESKTOP-B2QSEQC\nanas <e214122sk@kurume-it.ac.jp>
2021-06-15 13:34:30 +0900

    logoのscene作成　フェードイン・フェードアウトの追加に失敗

---
+++ Assets/logosystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class logosystem : MonoBehaviour
+{
+
+    public float speed = 0.01f;  //透明化の速さ
+    float alfa;    //A値を操作するための変数
+    float red, green, blue;    //RGBを操作するための変数
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("ChangeScene", 3.0f);
+        //Panelの色を取得
+        //red = GetComponent<Image>().color.r;
+        //green = GetComponent<Image>().color.g;
+        //blue = GetComponent<Image>().color.b;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GetComponent<Image>().color = new Color(red, green, blue, alfa);
+        //alfa += speed;
+    }
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("title");
+    }
+}
