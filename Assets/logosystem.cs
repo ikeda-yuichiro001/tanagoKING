ae2c00890ec92a4e3a99ccb14d23b52788426eae
DESKTOP-B2QSEQC\nanas <e214122sk@kurume-it.ac.jp>
2021-06-16 20:47:23 +0900

    フェードイン

--- Assets/logosystem.cs
+++ Assets/logosystem.cs
@@ -1,31 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class logosystem : MonoBehaviour
 {
 
     public float speed = 0.01f;  //透明化の速さ
+    public float antispeed = 0.02f;
     float alfa;    //A値を操作するための変数
     float red, green, blue;    //RGBを操作するための変数
+    Text text;
 
     // Use this for initialization
     void Start()
     {
+
         Invoke("ChangeScene", 3.0f);
-        //Panelの色を取得
-        //red = GetComponent<Image>().color.r;
-        //green = GetComponent<Image>().color.g;
-        //blue = GetComponent<Image>().color.b;
+       
+
+        text = GameObject.Find("Canvas/Text").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //GetComponent<Image>().color = new Color(red, green, blue, alfa);
+        text.color = new Color(red,green,blue,alfa);
         alfa += speed;
     }
+
+    
+
+
+
     void ChangeScene()
     {
         SceneManager.LoadScene("title");
